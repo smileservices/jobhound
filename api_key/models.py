92d512a8175d7b20301755e60b97ea309b3915a7
vladimir gorea <vladimir.gorea@gmail.com>
2019-05-19 17:30:27 +0700

    reset api_key

--- api_key/models.py
+++ api_key/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from api_user.models import ApiUser
+import secrets
 
 
 # Create your models here.
@@ -7,3 +8,7 @@ from api_user.models import ApiUser
 class APIKey(models.Model):
     user = models.ForeignKey(ApiUser, on_delete=models.CASCADE)
     api_key = models.CharField(max_length=128)
+
+    def generate_key(self):
+        key = secrets.token_urlsafe(16)
+        return key
--- api_key/views.py
+++ api_key/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import UpdateView
 from django.template import loader
 from api_key.models import APIKey
@@ -9,13 +9,13 @@ from api_key.forms import ApiKeyForm
 
 class ApiKeyUpdate(UpdateView):
     model = APIKey
-    template_name = 'dashboard/form.html'
+    template_name = 'dashboard/key.html'
     form_class = ApiKeyForm
     login_url = 'api_user-login'
 
     def get_object(self, queryset=None):
         return self.model.objects.get(user_id=self.request.user.id)
-    
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ApiKeyUpdate, self).get_context_data(object_list=None, **kwargs)
         context['page'] = {
@@ -24,3 +24,9 @@ class ApiKeyUpdate(UpdateView):
             'bc2': 'API Key',
         }
         return context
+
+    def post(self, request, *args, **kwargs):
+        object = self.get_object()
+        object.api_key = object.generate_key()
+        object.save()
+        return redirect(self.success_url)
